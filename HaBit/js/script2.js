bb5ae457d9e0e27dff4b65aca07025158b19db15
Jelle <j.vanlieshout@student.tudelft.nl>
2017-12-22 15:13:25 +0100

    kek

--- HaBit/js/script2.js
+++ HaBit/js/script2.js
@@ -346,3 +346,25 @@ function editHabit(index) {
     catalogus.getHabit(index).displayHabit(false, index);
 
 }
+
+
+//node.js
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "habits"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
